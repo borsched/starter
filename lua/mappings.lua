b347643160ddad6d21102482fc4f0f2fad810224
borsched <75818517+borsched@users.noreply.github.com>
2024-08-25 15:40:07 -0500

    Update mappings.lua
    Add compile and preview mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,5 +7,7 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("n", "<C-q>", ":quitall <cr>")
+map("n", ",c", ':w! | !compiler "%:p"<cr>')
+map("n", ",p", ':!opout "%:p"<cr>')
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
